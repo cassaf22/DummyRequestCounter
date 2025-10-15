29cddbb3ea754e3afa27c2a3141254d864491ba3
Charbel Assaf <charbel.assaf3@usj.edu.lb>
2025-10-15 10:04:58 +0300

    fix: remove unused import and correct blank lines

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import redis
-import os
 
 app = Flask(__name__)
 
@@ -10,11 +9,12 @@ r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 APP_PORT = int(os.getenv("APP_PORT", "8000"))
 
+
 @app.route("/")
 def hello():
     hits = r.incr("hits")
     return f"Hello! This page has been visited {hits} times."
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=APP_PORT, debug=True)
-
